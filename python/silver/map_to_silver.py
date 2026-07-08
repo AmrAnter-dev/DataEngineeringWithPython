37cd2a5b910f6da4c8fa601287aee90c9401494d
AmrAnter-dev <mavireck1983@gmail.com>
2026-07-08 13:14:07 +0300

    Update map_to_silver.py

--- python/silver/map_to_silver.py
+++ python/silver/map_to_silver.py
@@ -1,75 +1,74 @@
 import json
+
+from pyspark.sql.functions import col, from_json
+
 from utils.spark_utils import SparkManager
+from utils.schema_utils import extract_schema
 from utils.cdc_processor import CDCProcessor
-from pyspark.sql.functions import *
-from delta.tables import DeltaTable
-spark=SparkManager('pipeline')
-with open('/home/amr/bronze/tables.json','r',encoding='utf-8') as f:
-    configs=json.load(f)
+from utils.merge_utils import merge_batch
 
-bronze_path='/home/amr/ven311/bronze'
-silver_path='/home/amr/ven311/silver'
+spark = SparkManager("silver_pipeline")
 
-def map_to_silver(batch_df, batch_id,table_name,merge_column):
-   
-    target_table = f'{silver_path}/{table_name}'
-    
-    if not DeltaTable.isDeltaTable(spark, target_table):
-        batch_df\
-        .write\
-            .format("delta")\
-            .mode("overwrite")\
-            .save(target_table)
-    else:
-        dt= DeltaTable.forPath(spark,target_table)
-        
-    
-        dt.alias("tgt").merge(
-            batch_df.alias("src"),
-            "tgt.{merge_column} = src.{merge_column}" 
-        ).whenMatchedUpdateAll(
-            condition = "src.op = 'u'"
-        ).whenMatchedDelete(
-          
-            condition = "src.op = 'd'"
-        ).whenNotMatchedInsertAll(
-            condition = "src.op = 'c' OR src.op = 'r'" 
-        ).execute()
+with open("/home/amr/bronze/tables.json", "r", encoding="utf-8") as f:
+    configs = json.load(f)
+
+bronze_path = "/home/amr/ven311/bronze"
+silver_path = "/home/amr/ven311/silver"
+
+queries = []
+
 for table_name, cfg in configs.items():
-    source_table=f'{bronze_path}/{table_name}'
-    target_table=f'{table_name}'
-    merge_column=cfg.get('mergeColumn','')
 
-    json_schema=extract_schema(source_table)
-   
-    bronze_df=(
-        spark
-        .readStream
-        .format('delta')
-        .load(source_table)
+    source_path = f"{bronze_path}/{table_name}"
+    target_path = f"{silver_path}/{table_name}"
+
+    merge_column = cfg.get("mergeColumn")
+
+    if not merge_column:
+        raise ValueError(f"{table_name} has no mergeColumn")
+
+    json_schema = extract_schema(source_path)
+
+    bronze_df = (
+        spark.readStream
+        .format("delta")
+        .load(source_path)
     )
-    
+
     parsed_df = (
         bronze_df
         .withColumn(
-            'parsed_data',
-            from_json(col('value').cast('string'), json_schema)
+            "parsed_data",
+            from_json(
+                col("value").cast("string"),
+                json_schema
+            )
         )
     )
 
     silver_df = CDCProcessor.transform(parsed_df)
 
-   
-  query = (
-  silver_df
-  .writeStream
-  .foreachBatch(lambda df, bid:
-      map_to_silver(df, bid, table_name, merge_column)) 
-  .options(**{
-      'checkpointLocation': f'/home/amr/checkpoints/silver/{table_name}'
-  })
-  .start()
-  )
-  
+    query = (
+        silver_df.writeStream
+        .foreachBatch(
+            lambda df, bid,
+           tp=target_path,
+           mc=merge_column:
+                merge_batch(
+                    spark=spark,
+                    batch_df=df,
+                    target_path=tp,
+                    merge_column=mc
+                )
+        )
+        .option(
+            "checkpointLocation",
+            f"/home/amr/checkpoints/silver/{table_name}"
+        )
+        .start()
+    )
 
-  query.awaitTermination()
+    queries.append(query)
+
+for query in queries:
+    query.awaitTermination()
